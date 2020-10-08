ac11d7c20ef1d34cc2a87e8d38815b3e83912df2
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2020-10-08 17:45:33 +0900

    5. Using onChanged to show input text

--- lib/main.dart
+++ lib/main.dart
@@ -3,69 +3,77 @@ import 'package:free_write/1.%20helloworld.dart';
 import 'package:free_write/2.%20Stateless%20Widgets.dart';
 import 'package:free_write/3.Buttons%20and%20StatefulWidgets.dart';
 import 'package:free_write/4-1.Simple%20routes%20using%20StatelessWidget.dart';
+import 'package:free_write/4-2.Routes%20using%20StatefulWidget.dart';
+import '5. Using onChanged to show input text.dart';
 
 void main() {
-  runApp(new MaterialApp(home: new FirstPage()));
+  runApp(new MaterialApp(home: new MyTextInput()));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: '너무 어렵다'),
-    );
-  }
-}
+// void main() {
+//   runApp(new MaterialApp(home: new FirstPage(), routes: <String, WidgetBuilder>{
+//     "/SecondPage": (BuildContext context) => new SecondPage()
+//   }));
+// }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.purple,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MyHomePage(title: '너무 어렵다'),
+//     );
+//   }
+// }
 
-  final String title;
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+//   final String title;
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
